4a78a4bcb76d1522c504bd0a264ab963e9610f73
Ильдар Сиразутдинов <i.sirazutdinov@pflb.ru>
2021-12-25 22:27:28 +0300

    first positive test (PositivePostsListTest) almost fine now + equals reworked in Post model

--- src/test/java/models/Post.java
+++ src/test/java/models/Post.java
@@ -3,6 +3,8 @@ package models;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import model.BaseModel;
 
+import java.util.Objects;
+
 public class Post extends BaseModel {
     @JsonProperty("id")
     public Integer id;
@@ -11,8 +13,21 @@ public class Post extends BaseModel {
     public Integer userId;
 
     @JsonProperty("title")
-    public Integer title;
+    public String title;
 
     @JsonProperty("body")
-    public Integer body;
+    public String body;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Post)) return false;
+        Post post = (Post) o;
+        return Objects.equals(id, post.id) && Objects.equals(userId, post.userId) && title.equals(post.title) && body.equals(post.body);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, userId, title, body);
+    }
 }
--- src/test/java/tests/jsonplaceholder_api/posts/positive/PositivePostsListTest.java
+++ src/test/java/tests/jsonplaceholder_api/posts/positive/PositivePostsListTest.java
@@ -1,9 +1,13 @@
 package tests.jsonplaceholder_api.posts.positive;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Severity;
 import io.qameta.allure.SeverityLevel;
 import io.qameta.allure.Story;
+import models.Post;
+import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.junit.jupiter.api.*;
 import tests.jsonplaceholder_api.posts.AbstractPostsTest;
 
@@ -12,6 +16,13 @@ import tests.jsonplaceholder_api.posts.AbstractPostsTest;
 @Severity(value = SeverityLevel.BLOCKER)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class PositivePostsListTest extends AbstractPostsTest {
+    private static TestData testData;
+    ObjectMapper mapper = new ObjectMapper();
+
+    @BeforeAll
+    public static void setup() {
+        testData = new TestData();
+    }
 
     @DisplayName("Проверка возврата статуса 200 при получении первого поста")
     @Order(1)
@@ -21,6 +32,7 @@ public class PositivePostsListTest extends AbstractPostsTest {
                 .then()
                 .assertThat()
                 .statusCode(200);
+        // TODO logs
     }
 
     @DisplayName("Проверка возврата статуса 200 при получении списка постов")
@@ -31,26 +43,55 @@ public class PositivePostsListTest extends AbstractPostsTest {
                 .then()
                 .assertThat()
                 .statusCode(200);
+        // TODO logs
     }
 
     @DisplayName("Проверка количества постов")
     @Order(3)
     @Test()
     public void positiveApiTestStep3() {
-
+        Post[] postList = getPostsList();
+        Assertions.assertEquals(100, postList.length);
+        // TODO logs
     }
 
     @DisplayName("Проверка содержимого первого поста")
     @Order(4)
     @Test()
-    public void positiveApiTestStep4() {
-
+    public void positiveApiTestStep4() throws JsonProcessingException {
+        Post post = getPostModel(1);
+        Assertions.assertTrue(post.equals(testData.firstPost));
+        // TODO logs
     }
 
     @DisplayName("Проверка содержимого последнего поста")
     @Order(5)
     @Test()
     public void positiveApiTestStep5() {
+        Post post = getPostModel(100);
+        Assertions.assertTrue(post.equals(testData.lastPost));
+        // TODO logs
+    }
+
+    private static class TestData {
+        Post firstPost = new Post();
+        {
+            firstPost.id = 1;
+            firstPost.userId = 1;
+            firstPost.title = "sunt aut facere repellat provident occaecati excepturi optio reprehenderit";
+            firstPost.body = "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum" +
+                    "\nreprehenderit molestiae ut ut quas totam" +
+                    "\nnostrum rerum est autem sunt rem eveniet architecto";
+        }
+
+        Post lastPost = new Post();
+        {
+            lastPost.id = 100;
+            lastPost.userId = 10;
+            lastPost.title = "at nam consequatur ea labore ea harum";
+            lastPost.body = "cupiditate quo est a modi nesciunt soluta\nipsa voluptas error itaque dicta in" +
+                    "\nautem qui minus magnam et distinctio eum\naccusamus ratione error aut";
+        }
 
     }
 }
