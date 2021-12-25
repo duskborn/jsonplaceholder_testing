3451ef10e9146481db3d05481f02d6547dc91376
Ильдар Сиразутдинов <i.sirazutdinov@pflb.ru>
2021-12-25 19:29:58 +0300

    first TC is started

--- src/test/java/tests/AbstractJsonPlaceholderTest.java
+++ src/test/java/tests/AbstractJsonPlaceholderTest.java
@@ -1,4 +1,41 @@
 package tests;
 
-public class AbstractJsonPlaceholderTest {
+import io.restassured.response.Response;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.Assert;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static io.restassured.RestAssured.given;
+
+public class AbstractJsonPlaceholderTest extends AbstractTestNGSpringContextTests {
+    String endpointsPath = "src/main/resources/endpoints.txt";
+
+    protected String getEndpoint() {
+        String endpoint = null;
+        try {
+            endpoint = new String(Files.readAllBytes(Paths.get(endpointsPath)), StandardCharsets.US_ASCII);
+        } catch (IOException e) {
+            Assert.fail("Ошибка: " + e.getMessage());
+            logger.error("Ошибка: " + e);
+        }
+        return endpoint;
+    }
+
+    protected Response getPosts() {
+        return given()
+                .baseUri(getEndpoint())
+                .when()
+                .get("/posts");
+    }
+
+    protected Response getPost(int id) {
+        return given()
+                .baseUri(getEndpoint())
+                .when()
+                .get("/posts/" + id);
+    }
 }
